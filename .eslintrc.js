59779c9222067d1b402c5a0aec3d706b12e7741f
Leonardo Gurgel <lgurgel@id.uff.br>
2020-10-21 16:29:52 -0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    indent: ['error', 2],
+    'space-before-function-paren': ['error', 'never']
+  }
+}
