858a9695c27588c855cf2236681790a82253434b
Leonardo Gurgel <lgurgel@id.uff.br>
2020-10-21 17:14:45 -0300

    add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,12 @@ module.exports = {
   env: {
     commonjs: true,
     es2021: true,
-    node: true
+    node: true,
+    'jest/globals': true
   },
+  plugins: [
+    'jest'
+  ],
   extends: [
     'standard'
   ],
